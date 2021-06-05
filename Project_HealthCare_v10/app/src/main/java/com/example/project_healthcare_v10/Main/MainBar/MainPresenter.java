99654a1aeb03726a2f72ea982667ae2a87e8b105
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2021-06-05 13:13:53 +0700

    Remove old version

--- Project_HealthCare_v10/app/src/main/java/com/example/project_healthcare_v10/Main/MainBar/MainPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.example.project_healthcare_v10.Main.MainBar;
-
-public class MainPresenter implements MainContract.Presenter {
-    private MainContract.View view;
-
-    public void setView(MainContract.View view) {
-        this.view = view;
-    }
-}
